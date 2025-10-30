63ef1695550f7f6d5014f648a2fbde5935b5a155
Balu Areti <101409985+balu1006@users.noreply.github.com>
2025-10-30 17:59:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import requests
 
 app = Flask(__name__)
 
-API_URL = "https://api.data.gov.in/resource/ee03643a-ee4c-48c2-ac30-9f2ff26ab722?api-key=579b464db66ec23bdd0000013f631c0b762045036d516ffdf0484608&format=json&filters%5Bstate_name%5D=TELANGANA&filters%5Bfin_year%5D=2023-2024"
+API_URL = "API-KEY HERE"
 
 @app.route("/", methods=["GET"])
 def index():
